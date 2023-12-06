c2a371ed5625afc602517e5c8725101e04469e80
Pancake <148641211+PancakeTaste@users.noreply.github.com>
2023-12-06 21:19:07 +0200

    Initial commit

---
+++ src/main/java/me/pancaketaste/dodgebolt/Dodgebolt.java
@@ -0,0 +1,17 @@
+package me.pancaketaste.dodgebolt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dodgebolt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
